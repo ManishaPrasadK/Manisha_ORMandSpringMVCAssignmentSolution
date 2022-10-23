f4d61852c129c4ec0f482bb607dd698fef6a233c
ManishaPrasadK <107794198+ManishaPrasadK@users.noreply.github.com>
2022-10-23 14:07:14 +0530

    Update MainController.java

--- GradedAssignment/src/main/java/com/greatlearning/MainController.java
+++ GradedAssignment/src/main/java/com/greatlearning/MainController.java
@@ -1,5 +1,14 @@
 package com.greatlearning;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MainController {
+	
+	@RequestMapping("/")
+	public String viewHome() {
+		return "home";
+	}
 
 }
